502543dd62131aab6831557b3abd858275159e9d
Rafael Vieira <rafael3do@hotmail.com>
2019-01-10 09:28:12 -0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+            Console.ReadKey();
+        }
+    }
+}
