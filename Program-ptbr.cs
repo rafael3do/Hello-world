667d976c231264070044320003a6768c543093d0
Rafael Vieira <rafael3do@hotmail.com>
2019-01-10 09:29:15 -0200

    Create Program-ptbr.cs

---
+++ Program-ptbr.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace Olá_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo.");
+            Console.ReadKey();
+        }
+    }
+}
